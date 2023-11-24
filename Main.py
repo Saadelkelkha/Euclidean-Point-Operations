7165de77f842699b1c6d8da2818b3397acba130b
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-11-24 23:14:13 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 # Import the Point class from the ex1 module
 from p import Point
 
+# Create a Point object with coordinates of constructor
+point0 = Point()
 # Create a Point object with coordinates (76, 47)
 point1 = Point(76, 47)
 
 # Print the Euclidean distance between the point and itself
-print("The distance is:", point1.distance(point1))
+print("The distance is:", point0.distance(point1))
 
 # Print the Euclidean norm of the point
 print("The norm is:", point1.norm())
