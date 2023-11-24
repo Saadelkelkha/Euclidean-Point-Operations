cd389d8023e2d6faef1da7712d6ae9621bdd6454
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-11-24 23:15:59 +0100

    Update p.py

--- p.py
+++ p.py
@@ -1,32 +1,32 @@
-class Point:
-    def __init__(self, x=0, y=0):
+class Point():
+    def __init__(self, x = 0, y = 0):
         # Constructor to initialize the Point object with x and y coordinates
         self.__x = x
         self.__y = y
 
-    def get_x(self):
+    def Get_x(self):
         # Getter method to retrieve the x-coordinate of the Point
         return self.__x
 
-    def get_y(self):
+    def Get_y(self):
         # Getter method to retrieve the y-coordinate of the Point
         return self.__y
 
-    def set_x(self, x):
+    def Set_x(self, x):
         # Setter method to set the x-coordinate of the Point
         self.__x = x
 
-    def set_y(self, y):
+    def Set_y(self, y):
         # Setter method to set the y-coordinate of the Point
         self.__y = y
 
-    def distance(self, point1):
+    def Distance(self, point1):
         # Method to calculate the Euclidean distance between two points
-        d = ((point1.get_x() - self.__x) ** 2 + (point1.get_y() - self.__y) ** 2) ** 0.5
+        d = ((point1.Get_x() - self.Get_x()) ** 2 + (point1.Get_y() - self.Get_y()) ** 2) ** 0.5
         return d
 
-    def norm(self):
-        # Method to calculate the Euclidean norm of the Point
+    def Norm(self):
+        # Method to cGlculate the Euclidean norm of the Point
         n = (self.__x ** 2 + self.__y ** 2) ** 0.5
         return n
 
